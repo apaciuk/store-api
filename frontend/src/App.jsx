6bdf5b89c9d407fc3ddf4fb56594404dda3c28dd
Paul Anthony McGowan <xhostcom@aol.com>
2023-02-16 10:28:04 +0000

    App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,32 +1,29 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [user, setUser] = useState({});
+  const clientId = ENV('CLIENT_ID')
+  const clientSecret = ENV('CLIENT_SECRET')
+
+  const login = () => {
+    const data = {
+      "client_id": clientId,
+      "client_secret": clientSecret,
+      "grant_type": "password",
+      "email": "test@test.com",
+      "password": "password",
+
+  }
+
 
   return (
     <div className="App">
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      Output
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    
     </div>
   )
 }
